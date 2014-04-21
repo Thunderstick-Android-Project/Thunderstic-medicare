0b9ce19031ca7e6387154516f38f784a2081f31d
tawaasalage <tawaasalage@gmail.com>
2014-04-20 22:20:28 -0700

    Project started

---
+++ MedReminder/src/com/thunderstick/medreminder/MainActivity.java
@@ -0,0 +1,16 @@
+package com.thunderstick.medreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+   
+    }
+
+   
+}
