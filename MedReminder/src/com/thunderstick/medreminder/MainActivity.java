bbe0dacdac5adc2abaf9665354bc6136d91a46a9
isururk <isururk@gmail.com>
2014-04-23 14:28:27 +0530

    test 5

--- MedReminder/src/com/thunderstick/medreminder/MainActivity.java
+++ MedReminder/src/com/thunderstick/medreminder/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
         
-        setContentView(R.layout.itemedit);
+        setContentView(R.layout.add);
         
         
         
