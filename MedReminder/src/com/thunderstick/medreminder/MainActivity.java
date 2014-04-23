04981a30dd24694fc8ddda43a72c6fdfe9081e51
tawaasalage <tawaasalage@gmail.com>
2014-04-23 14:29:35 +0530

    Front page redirection done

--- MedReminder/src/com/thunderstick/medreminder/MainActivity.java
+++ MedReminder/src/com/thunderstick/medreminder/MainActivity.java
@@ -1,23 +1,33 @@
 package com.thunderstick.medreminder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-	
-	
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainpage);
-        
-        setContentView(R.layout.itemedit);
-        
-        
-        
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainpage);
 
-   
+		Thread timer = new Thread() {
+
+			public void run() {
+				try {
+					sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent openMainActivity = new Intent("com.thunderstick.medreminder.add");
+					startActivity(openMainActivity);
+				}
+			}
+		};
+		timer.start();
+
+
+	}
+
 }
